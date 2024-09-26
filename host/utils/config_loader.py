bffed6e9d634e05a23a8b6234b5d9c0d69f96e69
Woodash <woodash830@outlook.com>
2024-09-26 18:01:31 +0800

    style: add comment & format code

--- host/utils/config_loader.py
+++ host/utils/config_loader.py
@@ -10,36 +10,46 @@ updatefreq: 30
 encodingkey: 'yourencodingkey'
 name: 'hostname'
 """
-class ConfigLoader():
+
+
+class ConfigLoader:
     _instance = None
     _init = False
-    def __init__(self, path = None):
-        DEFAULT_CONFIG_PATH = 'A.yaml'
-        if self._init is True and path is None:
+
+    def __init__(self, path=None):
+        if self._init and path:
             return
+
+        DEFAULT_CONFIG_PATH = "A.yaml"
         self._init = True
-        if path is not None:
+
+        if path:
             self.config_path = path
-        if hasattr(self, "config_path") is False:
+
+        if not hasattr(self, "config_path"):
             self.config_path = DEFAULT_CONFIG_PATH
+
         self.config = self.load_yaml()
-  
 
     def __new__(cls, *args, **kw):
-        if cls._instance is None:
+        if not cls._instance:
             cls._instance = super(ConfigLoader, cls).__new__(cls)
+
         return cls._instance
 
     def load_yaml(self):
-        if os.path.exists(self.config_path) == False:
+        path_exist = os.path.exists(self.config_path)
+
+        if not path_exist:
             my_uid = uuid.uuid4()
             config = yaml.load(example_doc, Loader=yaml.Loader)
-            config['server'].update({'uid': str(my_uid) })
+            config["server"].update({"uid": str(my_uid)})
             tmp_doc = yaml.dump(config)
-            with open(self.config_path,'w+') as f:
+            with open(self.config_path, "w+", encoding="utf-8") as f:
                 f.write(tmp_doc)
-        else:
-            with open(self.config_path, 'r') as f:
-                config = yaml.load(f, Loader=yaml.Loader)
-        return config
 
+        else:
+            with open(self.config_path, "w+", encoding="utf-8") as f:
+                config = yaml.load(f, Loader=yaml.Loader)
+                
+        return config
--- host/utils/crypto_util.py
+++ host/utils/crypto_util.py
@@ -1,52 +1,58 @@
 from Crypto.Cipher import AES
-from Crypto.Util.Padding import pad,unpad
+from Crypto.Util.Padding import pad, unpad
 from Crypto.Hash.SHA256 import SHA256Hash
 import base64
 
 
-class CryptoUtil():
-    '''
+class CryptoUtil:
+    """
     加密工具,使用AES_CBC
-    '''
-    _instance = None
+    """
+
+    _instance = None    
     _init = False
-    def __init__(self, config = None):
+
+    def __init__(self, config=None):
         if self._init is True and config is None:
             return
+        
         self._init = True
-        if hasattr(self, "config") is False:
+        if not hasattr(self, "config"):
             self.config = config
-        self.key =SHA256Hash(self.config['encodingkey'].encode()).digest()[:16]
+
+        self.key = SHA256Hash(self.config["encodingkey"].encode()).digest()[:16]
 
     def __new__(cls, *args, **kw):
-        if cls._instance is None:
+        if not cls._instance:
             cls._instance = super(CryptoUtil, cls).__new__(cls)
         return cls._instance
 
-    def encode(self, text)->str:
-        if text is None:
-            return text
-        iv = self.key # iv偏移量，bytes类型
-        byte_text = pad(text.encode('utf-8'), AES.block_size)
-        aes = AES.new(self.key,AES.MODE_CBC,iv)
+    def encode(self, text) -> str:
+        if not text:
+            return
+        iv = self.key  # iv偏移量，bytes类型
+        byte_text = pad(text.encode("utf-8"), AES.block_size)
+        aes = AES.new(self.key, AES.MODE_CBC, iv)
         encrypt_text = aes.encrypt(byte_text)
-        return base64.encodebytes(encrypt_text).decode('utf-8')
+        return base64.encodebytes(encrypt_text).decode("utf-8")
 
-    def decode(self, encrypt_text)->str:
-        if encrypt_text is None:
+    def decode(self, encrypt_text) -> str:
+        if not encrypt_text:
             return encrypt_text
+        
         iv = self.key
-        encrypt_text = base64.decodebytes(encrypt_text.encode('utf-8'))
-        aes = AES.new(self.key,AES.MODE_CBC,iv)
+        encrypt_text = base64.decodebytes(encrypt_text.encode("utf-8"))
+        aes = AES.new(self.key, AES.MODE_CBC, iv)
         text = aes.decrypt(encrypt_text)
         text = unpad(text, AES.block_size)
-        return text.decode('utf-8')
+        return text.decode("utf-8")
+
 
 if __name__ == "__main__":
-    config = {'key':"asd"}
+    config = {"key": "asd"}
     res = {}
-    m = 'abcdeasdasdfg'
+    m = "abcdeasdasdfg"
     a = CryptoUtil(config)
-    k =a.encode(m)
-    res['a'] = k
+    k = a.encode(m)
+    res["a"] = k
     print(res)
